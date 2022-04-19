51a21d28ff30ea0776708569fbb2dc24de59866a
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-04-19 05:12:11 +0300

    initial commit

---
+++ src/config/firebaseConfig.ts
@@ -0,0 +1,12 @@
+import { FirebaseOptions } from "firebase/app";
+
+export const firebaseConfig: FirebaseOptions = {
+    apiKey: "AIzaSyBkE_ZIa1ilUG4gc0B9_CZwir_4CgbaGS8",
+    authDomain: "topic-app-ikyilmaz.firebaseapp.com",
+    projectId: "topic-app-ikyilmaz",
+    storageBucket: "topic-app-ikyilmaz.appspot.com",
+    messagingSenderId: "572057633281",
+    appId: "1:572057633281:web:6e95a47cac3a606c707600",
+    measurementId: "G-98DD02PLKW"
+};
+
